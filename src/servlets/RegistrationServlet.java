2e08d54a85a871ca226cea2bff9eb525a74e4771
Jannik <Jannik@193.22.74.99>
2016-10-20 11:15:37 +0200

    Registrierung läuft... ohne Rückmeldung des Status

--- src/servlets/RegistrationServlet.java
+++ src/servlets/RegistrationServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import classes.Adresse;
 import classes.SqlConnection;
+import classes.User;
+
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.UUID;
@@ -43,23 +46,34 @@ public class RegistrationServlet extends HttpServlet {
         if (act == null) {
             //no button has been selected
         } else if (act.equals("registrieren")) {
+        	
+        	if(pass1.equals(pass2))
+        	{
 
-            try {
+             try {
             
-             int adrId= UUID.randomUUID().hashCode();
-             Adresse kundenAdresse = new Adresse(adrId, strasse, hausnummer, plz, ort);
+            User user = new User(0, mail, vname, nname, pass1, rolle, 0, strasse, hausnummer, plz ,ort);
+            user.createUserInDB();
+            
+            String nextJSP = "/index.jsp";
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+            dispatcher.forward(request,response);
 
-            } catch (Exception ex) {
-ex.printStackTrace();
-            }
+             } catch (Exception ex) {
+            	ex.printStackTrace();
+             }
+        	}
+        	else
+        	{
+        		
+        	}
 
         }
 
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
+    	request.setAttribute("statusLabel", request.getParameter("hallo"));
     }
 
 }
