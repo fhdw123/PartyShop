feaf98319472815c4b5a271f98d8b1197bd3e8ca
fhdw123 <jannik.reiffer@web.de>
2016-10-29 21:11:12 +0200

    Update CategoryServlet.java

--- src/servlets/CategoryServlet.java
+++ src/servlets/CategoryServlet.java
@@ -41,14 +41,9 @@ public class CategoryServlet extends HttpServlet {
 			request.setAttribute("kategorien", kategorien);
 			ArrayList<Artikel> artikel = conn.showArtikelsInKategorie(request.getParameter("id"));
 			request.setAttribute("artikel", artikel);
-			
-			
-			
-			
-			
 			String nextJSP = "/kategorie.jsp";
-            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
-            dispatcher.forward(request,response);
+            		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+            		dispatcher.forward(request,response);
 		}
 		catch(Exception e)
 		{
