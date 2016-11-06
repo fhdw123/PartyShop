4ca3f47ad81a2a1b7f5c8198b42ddcf513f70be4
fhdw123 <jannik.reiffer@web.de>
2016-11-06 19:27:25 +0100

    Add files via upload

---
+++ src/servlets/CartServlet.java
@@ -0,0 +1,76 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import classes.Artikel;
+import classes.Position;
+import classes.SqlConnection;
+
+/**
+ * Servlet implementation class CartServlet
+ */
+@WebServlet("/Warenkorb")
+public class CartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CartServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try
+		{
+			SqlConnection conn = new SqlConnection();
+			HttpSession session=request.getSession(false);  
+	        if(session!=null) 
+	        {
+	        	ArrayList<Position> cart = new ArrayList<Position>();
+	        	if(session.getAttribute("cart") != null)
+	        	{
+	        		cart = (ArrayList<Position>) session.getAttribute("cart");
+	        		request.setAttribute("cart", cart);
+	        	}
+	        	else
+	        	{
+	        		request.setAttribute("cart", cart);
+	        	}
+	        	
+	        }
+	        String nextJSP = "/warenkorb.jsp";
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+            dispatcher.forward(request,response);
+	        
+	        
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	        
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
