abb1ffda00003594d6300668b80e2de6b7ea28d7
fhdw123 <jannik.reiffer@web.de>
2016-11-06 16:00:04 +0100

    Update SearchServlet.java

--- src/servlets/SearchServlet.java
+++ src/servlets/SearchServlet.java
@@ -1,14 +1,26 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import classes.Artikel;
+import classes.Kategorie;
+import classes.SqlConnection;
+
 /**
  * Servlet implementation class SearchServlet
  */
+@WebServlet("/Suche")
 public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -25,7 +37,64 @@ public class SearchServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try
+		{
+
+			SqlConnection conn = new SqlConnection();
+			ArrayList<Kategorie> kategorien = conn.showKategorien();
+			request.setAttribute("kategorien", kategorien);
+			ArrayList<Artikel> artikel = conn.showAllArtikels();
+			
+			
+			
+			String[] kategorieFilter;
+			if((kategorieFilter = request.getParameterValues("kat")) != null)
+			{
+				HashMap<String, String> kategorienIds = new HashMap<String, String>();
+				for(Kategorie kategorie: kategorien)
+				{
+					kategorienIds.put(kategorie.getKategorieid(), kategorie.getBezeichnung());
+				}
+			
+				ArrayList<Artikel> artikelCopy = new ArrayList<Artikel>();
+				for(Artikel art: artikel)
+				{
+					boolean contains = false;
+					for(String filter: kategorieFilter)
+					{
+						
+						if(kategorienIds.get(art.getKategorie()).equals(filter))
+						{
+							contains = true;
+						}
+					}
+					if(contains)
+					{
+						artikelCopy.add(art);
+					}
+					
+				}
+				for(String filter: kategorieFilter)
+				{
+					request.setAttribute(filter + "checked", "checked=\"checked\"");
+				}
+				artikel = artikelCopy;
+
+				
+			}
+			request.setAttribute("artikel", artikel);
+			conn.closeConnection();
+			
+			
+			String nextJSP = "/suche.jsp";
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+            dispatcher.forward(request,response);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			
+		}
 	}
 
 	/**
