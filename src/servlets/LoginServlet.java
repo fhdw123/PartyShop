ca45eb047e90572c2cc10d4e5003da68ab3bb968
fhdw123 <jannik.reiffer@web.de>
2016-11-12 12:25:47 +0100

    Add files via upload

--- src/servlets/LoginServlet.java
+++ src/servlets/LoginServlet.java
@@ -1,11 +1,8 @@
 package servlets;
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -19,10 +16,13 @@ import classes.PasswortVerschluesselung;
 import classes.SqlConnection;
 import classes.User;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/ServletLogin")
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
@@ -30,25 +30,21 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String nextJSP = "/login.jsp";
+
+		String nextJSP = "/loginregister.jsp";
     	RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
     	dispatcher.forward(request,response);
-
 
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-
-		PrintWriter out = response.getWriter();
+		// TODO Auto-generated method stub
 		String mail = request.getParameter("mail");
 		String pass = request.getParameter("pass");
 
 		String act = request.getParameter("act");
-		if (act == null) {
-			// no button has been selected
-		} else if (act.equals("login")) {
+	  if (act.equals("login")) {
 
 			try {
 
@@ -98,10 +94,101 @@ public class LoginServlet extends HttpServlet {
 				// Mail Adresse nicht vorhanden
 				ex.printStackTrace();
 			}
-	
+
+		}
+		else if(act.equals("registrieren"))
+		{
+			mail = request.getParameter("mail").toLowerCase();
+			String vname = request.getParameter("vn");
+			String nname = request.getParameter("nn");
+			String pass1 = request.getParameter("pw");
+			String pass2 = request.getParameter("pw2");
+			String rolle = "kunde";
+			String strasse = request.getParameter("str");
+			String hausnummer = request.getParameter("hn");
+			String plz = request.getParameter("plz");
+			String ort = request.getParameter("ort");
+
+		
+
+				if (!mail.isEmpty() && !vname.isEmpty() && !nname.isEmpty() && !pass1.isEmpty() && !pass2.isEmpty()
+						&& !rolle.isEmpty() && !strasse.isEmpty() && !hausnummer.isEmpty() && !plz.isEmpty()
+						&& !ort.isEmpty()) {
+
+					if (mail.contains("@") && mail.contains(".")) {
+
+						if (isPlz(plz)) {
+
+							if (pass1.equals(pass2)) {
+
+								try {
+
+									PasswortVerschluesselung pv = new PasswortVerschluesselung();
+									User user = new User(mail, vname, nname, pv.SHA512(pass1), rolle, 0, strasse, hausnummer,
+											Integer.parseInt(plz), ort);
+									user.userAnlegen();
+
+
+									if (rolle.equals("kunde")) {
+
+										SqlConnection con = new SqlConnection();
+
+										HttpSession session = request.getSession(false);
+										session.setAttribute("user", user);
+
+										response.sendRedirect("IndexServlet");
+
+									} else if (rolle.equals("mitarbeiter")) {
+										
+										response.sendRedirect("MitarbeiterBereichServlet");
+
+									} else if (rolle.equals("administrator")) {
+										
+										response.sendRedirect("AdminBereichServlet");
+
+									}
+
+								} catch (Exception ex) {
+									ex.printStackTrace();
+								}
+							} else {
+								// Ausgabe: Passwörter stimmen nicht überein
+							}
+
+						} else {
+							// Ausgabe: Postleitzahl ungültig
+						}
+					} else {
+						// Ausgabe: bitte eine gültige Mailadresse angeben
+
+					}
+				} else {
+					// Ausgabe: Daten fehlen
+				}
+		}
+
+			
 
 		}
 
-	}
+		/**
+		 * 
+		 * @param i
+		 * @return
+		 */
+		public boolean isPlz(String i) {
+			try {
+				int num = Integer.parseInt(i);
+				if (i.length() == 5) {
+					return true;
+				} else {
+					return false;
+				}
+
+			} catch (NumberFormatException e) {
+				return false;
+			}
+
+		}
 
 }
